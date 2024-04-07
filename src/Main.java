5ba260794a4c36279d0bb30e977feabdd39e2984
Galymzhan1626 <154108707+Galymzhan1626@users.noreply.github.com>
2024-04-08 01:45:20 +0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * This method calculates the factorial of a given number 'n'.
+     * It uses recursion to perform the calculation, multiplying 'n' by the factorial of 'n-1'.
+     * The base case is when 'n' is 0, where it returns 1.
+     *
+     * @param n The non-negative integer for which the factorial is to be calculated.
+     * @return The factorial of 'n'.
+     */
     public static int factorial(int n){
         if(n==0){
             return 1;
